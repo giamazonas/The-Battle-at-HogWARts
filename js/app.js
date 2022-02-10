88f1e7ad1202d8613e1f12c285781d3f25a037de
giamazonas <skibinskirenee@gmail.com>
2022-02-10 13:04:03 -0500

    add initial info

---
+++ js/app.js
@@ -0,0 +1,57 @@
+//**  */  Declare deck variables
+let wizardA, wizardB
+let winner, battlefield, tie, cardStack, wand, tieBattle, endGame, 
+// tieWandClick  ??
+
+
+//** */ Cached element references
+
+
+//** */  Event listeners
+
+// handleClickWand (play card to battlefield), 
+// handleClickReplay(reset button)
+// handleClickCollect
+// handleclickStart (choose wizard?)
+
+//** */ Functions
+
+
+// function init()
+// function checkCardsValue()
+// function render()
+// function winner()
+// function compare()
+// function Tie
+// function Victory
+
+
+
+
+
+
+//**   extra */
+// gamePlay:
+// function for random card draw
+// card values
+// card select random
+
+// turn function
+// play one card each,
+// compare function, declare winner
+// else if Tie
+// play three cards, two face down and one up
+// compare function
+
+// after winner declared, return all cards to winner's collect pile
+
+// clickWand starts turn,
+// flip one random card on to battlefield
+// unless triggered by Tie, tie click flips 3.
+
+// collect button appears when winner of round declared
+// takes all cards on battlefield and adds to winners cardstack (randomly?)
+
+// Victory function: when no cards remain on battlefield and one wizard has no cards to play, victory declared
+// announce wizard with all cards with message
+// announce death of other wizard?
