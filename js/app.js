f4686f59a25cea12aea68fce68bd9c86723906ea
giamazonas <skibinskirenee@gmail.com>
2022-02-11 12:57:30 -0500

    add shuffle

--- js/app.js
+++ js/app.js
@@ -2,24 +2,24 @@ console.log(`sanity check`)
 
 // //**  */  Declare deck variables
 let wizard1, wizard2
-let winner, battlefield, cardStack, wand, tieBattle, endGame
+let winner, battlefield, cardStack, wand, tieBattle, endGame, deck
 // tieWandClick  ??
 
 
 //** */ Cached element references
-let deckBF1 = []
-let deckBF2 = []
-// let wiz1cards = []
-// let wiz1cards = []
+// let deckBF1 = []
+// let deckBF2 = []
+
 // let deck-start1 = []
 // let deck-start2 = []
 
 
 let battleCard1 = document.getElementById('deckBF1')
-let battleCard2 = document.getElementById('deckBF22')
+let battleCard2 = document.getElementById('deckBF2')
 
-let wiz1cards = document.getElementById('deck-start1')
-let  wiz2cards= document.getElementById('deck-start2')
+
+let wiz1Cards = document.getElementById('deck-start1')
+let wiz2Cards = document.getElementById('deck-start2')
 let wandW1 = document.getElementById(btnW1)
 let wandW2 = document.getElementById(btnW2)
 
@@ -32,6 +32,7 @@ document.getElementById('btnW1').addEventListener('click', ()=> console.log('cli
 
 document.getElementById('btnW2').addEventListener('click', ()=> console.log('clicked'))
 
+
 // handleClickWand (play card to battlefield), 
 // handleClickReplay(reset button)
 // handleClickCollect
@@ -39,65 +40,91 @@ document.getElementById('btnW2').addEventListener('click', ()=> console.log('cli
 
 //** */ Functions
 
-// Init()
 
+// shuffle 
+// split card array into two arrays
+// random assortment - half deck - into each
+
+init()
 function init() {
-  ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
-}
+  const deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+  let wiz1Cards = []
+  let wiz2Cards = []
+  if (deck.length > 0){
+    let randIdx = Math.floor(Math.random()*deck.length)
+    deck.sort(function(){
+      return 0.5 - Math.random()})
+    let cardsPicked = deck.splice(0, 26)
 
-function handleClick1(){
-// to prevent error on click when no cards are left
-if (deckBF1.length > 0){
-  let randIdx = Math.floor(Math.random()*deckBF1.length)
-  let cardPicked1 = deckBF1.splice(randIdx, 1)
-  battleCard1.push(cardPicked1)
-  render(cardPicked1)
-} 
-}
+    return wiz1Cards.push(cardsPicked)
+    return wiz2Cards = wiz2Cards.push(deck)
+}}
 
-function handleClick2(){
-  // to prevent error on click when no cards are left
-  if (deckBF2.length > 0){
-    let randIdx = Math.floor(Math.random()*deckBF1.length)
-    let cardPicked2 = deckBF2.splice(randIdx, 1)
-    battleCard2.push(cardPicked2)
-    render(cardPicked2)
-  } 
-  }
+// console.log(cardsPicked)
+console.log('OG DECK', deck)
+console.log('FIRST DECK', wiz1Cards)
+console.log('SECOND DECK', wiz2Cards)
 
 
-function cardPicked1(){
-  // randomly select from card in their own pile
-  renderCompare() 
-  //assign card with random index to a variable
-  //add card picked to battleCard area
-  //pass card to renderCompare function to display
-}
-function cardPicked2(){
-  // randomly select from card in their own pile
-  renderCompare() 
-  //assign card with random index to a variable
-  //add card picked to battleCard area
-  //pass card to renderCompare function to display
-}
+// specify sedond deck is 26 in length
+// let deck two equal (change name )
+
+// let b = deck.splice(0,26);
+// console.log(b)
+
+// function handleClick1(){
+// // to prevent error on click when no cards are left
+// if (deckBF1.length > 0){
+//   let randIdx = Math.floor(Math.random()*deckBF1.length)
+//   let cardPicked1 = deckBF1.splice(randIdx, 1)
+//   battleCard1.push(cardPicked1)
+//   render(cardPicked1)   
+//   } 
+// }
+
+// function handleClick2(){
+//   // to prevent error on click when no cards are left
+//   if (deckBF2.length > 0){
+//     let randIdx = Math.floor(Math.random()*deckBF1.length)
+//     let cardPicked2 = deckBF2.splice(randIdx, 1)
+//     battleCard2.push(cardPicked2)
+//     render(cardPicked2)
+//   } 
+// }
 
 
-function renderCompare() {
-  //cards picked by 1 & 2 compaired
-  if (cardPicked2 > cardPicked1){
-    let (cardToRemove = cardPicked1 && cardPicked2)
-    render wiz2Cards.push(cardToRemove)
-  }else if (cardPicked1 > cardPicked2){
-    let (cardToRemove = cardPicked1 && cardPicked2)
-    render wiz2Cards.push(cardToRemove)
-  }else if (cardPicked1 === cardPicked2) {
-    render tiePlay()
-  }
+// function cardPicked1(){
+//   // randomly select from card in their own pile
+//   renderCompare() 
+//   //assign card with random index to a variable
+//   //add card picked to battleCard area
+//   //pass card to renderCompare function to display
+// }
+// function cardPicked2(){
+//   // randomly select from card in their own pile
+//   renderCompare() 
+//   //assign card with random index to a variable
+//   //add card picked to battleCard area
+//   //pass card to renderCompare function to display
+// }
+
+
+// function renderCompare() {
+//   //cards picked by 1 & 2 compaired
+//   if (cardPicked2 > cardPicked1){
+//     let (cardToRemove = cardPicked1 && cardPicked2)
+//     render wiz2Cards.push(cardToRemove)
+//   }else if (cardPicked1 > cardPicked2){
+//     let (cardToRemove = cardPicked1 && cardPicked2)
+//     render wiz2Cards.push(cardToRemove)
+//   }else if (cardPicked1 === cardPicked2) {
+//     render tiePlay()
+//   }
   // if > wins, return message, pull cards to winners pile
   // else if tie, return tie message, render tiePlay function
   // lose(,,,? probably cue victory function)
   // render (init) OR gameState ? might be card picked...
-}
+// }
 
 
 function tiePlay(){
@@ -112,7 +139,7 @@ function victory(){
 // }else if (wiz2cards === 0 && battleCard2 ===0 {
 //   console.log(`${wizard1} has defeated Voldemort! ${wizard2} died bravely in battleCard1.`)
 //   }
-// }
+}
 
 // function cardToRemove()
 
