abdd99feae4351a22354919d558c1ccc3ac6c25e
giamazonas <skibinskirenee@gmail.com>
2022-02-11 16:04:55 -0500

    add object

--- js/app.js
+++ js/app.js
@@ -11,9 +11,9 @@ let wiz1Cards = []
 let wiz2Cards = []
 let cardsToRemove = []
 
-// let deck-start1 = []
-// let deck-start2 = []
-
+let decK = {
+  dA: 1, dQ: 12, dK: 13, dJ: 11, d10: 10, d09: 9, d08: 8, d07: 7, d06: 6, d05: 5, d04: 4, d03: 3, d02: 2, hA: 1, hQ: 12, hK: 13, hJ: 11, h10: 10, h09: 9, h08: 8, h07: 7, h06: 6, h05: 5, h04: 4, h03: 3, h02: 2, cA: 1, cQ:12, cK: 13, cJ: 11, c10: 10, c09: 9, c08: 8, c07: 7, c06: 6, c05: 5, c04: 4, c03: 3,c02: 2, sA: 1, sQ: 12, sK: 13, sJ: 11, s10: 10, s09: 9, s08: 8, s07: 7, s06: 6, s05: 5, s04: 4, s03: 3, s02: 2,
+}
 
 let battleCard1 = document.getElementById('deckBF1')
 let battleCard2 = document.getElementById('deckBF2')
@@ -66,10 +66,8 @@ console.log('SECOND DECK', wiz2Cards)
 
 // specify sedond deck is 26 in length
 // let deck two equal (change name )
-
 // let b = deck.splice(0,26);
 // console.log(b)
-
 function handleClick1(){
 // to prevent error on click when no cards are left
 // when wand is clicked, pick card from wiz_Cards
@@ -95,6 +93,46 @@ if (wiz2Cards.length > 0){
 }}
 console.log(`wz hand 2`, cardPicked2)
 
+
+function renderCompare() {
+  //cards picked by 1 & 2 compaired
+  if (cardPicked2 > cardPicked1){
+    let (cardsToRemove = cardPicked1 && cardPicked2)
+    return wiz2Cards.push(cardsToRemove)
+    return message `${wizard2} takes the advantage`
+  }else if (cardPicked1 > cardPicked2){
+    let (cardsToRemove = cardPicked1 && cardPicked2)
+    return wiz1Cards.push(cardsToRemove)
+    return message `${wizard1} takes the advantage`
+  }else if (cardPicked1 === cardPicked2) {
+    return tiePlay()
+    return message `It's a tie...strike again!`
+  }
+  // if > wins, return message, pull cards to winners pile
+  // else if tie, return tie message, render tiePlay function
+  // lose(,,,? probably cue victory function)
+  // render (init) OR gameState ? might be card picked...
+}
+
+
+function tiePlay(){
+  // hidden button appears?  
+  // plays three cards, two face down, one up
+  // renderCompare()
+  battleCard1.push(cardPicked1)
+  battleCard2.push(cardPicked2)
+}
+
+function victory(){
+// if (wiz1cards === 0 && battleCard1 ===0) {
+//   console.log(`${wizard2} is the victor! ${wizard1} died bravely in battle.`)
+// }else if (wiz2cards === 0 && battleCard2 ===0 {
+//   console.log(`${wizard1} has defeated Voldemort! ${wizard2} died bravely in battleCard1.`)
+//   }
+}
+
+// function cardToRemove()
+
 // function cardPicked1(){
 //   // randomly select from card in their own pile
   // renderCompare() 
@@ -109,41 +147,6 @@ console.log(`wz hand 2`, cardPicked2)
 //   //add card picked to battleCard area
 //   //pass card to renderCompare function to display
 // }
-
-
-function renderCompare() {
-  //cards picked by 1 & 2 compaired
-  if (cardPicked2 > cardPicked1){
-    let (cardsToRemove = cardPicked1 && cardPicked2)
-    return wiz2Cards.push(cardsToRemove)
-  }else if (cardPicked1 > cardPicked2){
-    let (cardsToRemove = cardPicked1 && cardPicked2)
-    return wiz1Cards.push(cardsToRemove)
-  }else if (cardPicked1 === cardPicked2) {
-    return tiePlay()
-  }
-  // if > wins, return message, pull cards to winners pile
-  // else if tie, return tie message, render tiePlay function
-  // lose(,,,? probably cue victory function)
-  // render (init) OR gameState ? might be card picked...
-}
-
-
-function tiePlay(){
-// render tie message
-// wiz play 3 cards each, render compare last card
-// 
-}
-
-function victory(){
-// if (wiz1cards === 0 && battleCard1 ===0) {
-//   console.log(`${wizard2} is the victor! ${wizard1} died bravely in battle.`)
-// }else if (wiz2cards === 0 && battleCard2 ===0 {
-//   console.log(`${wizard1} has defeated Voldemort! ${wizard2} died bravely in battleCard1.`)
-//   }
-}
-
-// function cardToRemove()
 
 //// function init()
 // function gameState
